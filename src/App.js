3a0caa054f999052e17e9e498b029d45311f3b53
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-08-01 00:15:44 +0200

    Basic structure part 2

--- src/App.js
+++ src/App.js
@@ -15,21 +15,17 @@ class App extends Component {
       <Router history={history}>
         <div className="App">
           <div className="container homeCNT">
-              <nav className="navCNT col-4">
-                <NavLink to="/" style={{ textDecoration: 'none'}} ><div className="menu-logo"><p>menu</p></div></NavLink>
-                <NavLink to="/nip-info" style={{ textDecoration: 'none'}} ><div className=""><p>Co to jest NIP?</p></div></NavLink>
-                <NavLink to="/krs-info" style={{ textDecoration: 'none' }} ><div className=""><p>Co to jest KRS?</p></div></NavLink>
-                <NavLink to="/regon-info" style={{ textDecoration: 'none'}} ><div className=""><p>Co to jest REGON?</p></div></NavLink>
+              <nav className=" leftSection col-5">
+                <div className="navCNT">
+                  <NavLink to="/" style={{ textDecoration: 'none'}} ><div className="menu-logo"><p>menu</p></div></NavLink>
+                </div>
+                <div className="rightSection col-7">
+                  <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route component={NoMatch}/>
+                  </Switch>
+                </div>
               </nav>
-              <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/nip-info" component={NipInfo}/>
-                <Route exact path="/krs-info" component={KrsInfo}/>
-                <Route exact path="/regon-info" component={RegonInfo}/>
-                <Route component={NoMatch}/>
-              </Switch>
-              <footer>
-              </footer>
           </div>
         </div>
       </Router>
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="mainInfoText">
+        <p>
+        </p>
+      </div>
+    );
+  }
+}
