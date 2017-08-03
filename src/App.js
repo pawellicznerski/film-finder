5bad2ce6adc09df1d2a6af3b5650b99ce15febf5
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-08-04 01:18:33 +0200

    Basic structure part 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router,Route,NavLink, Switch } from 'react-router-dom';
 import './App.css';
 import { Home }  from './components/home.js';
+import { Form }  from './components/form.js';
 import { NipInfo }  from './components/nipInfo.js';
 import { KrsInfo }  from './components/krsInfo.js';
 import { RegonInfo }  from './components/regonInfo.js';
@@ -14,18 +15,33 @@ class App extends Component {
     return (
       <Router history={history}>
         <div className="App">
-          <div className="container homeCNT">
-              <nav className=" leftSection col-5">
+          <div className="container">
+              <nav>
                 <div className="navCNT">
                   <NavLink to="/" style={{ textDecoration: 'none'}} ><div className="menu-logo"><p>menu</p></div></NavLink>
                 </div>
-                <div className="rightSection col-7">
+              </nav>
+              <section>
+                <div className="mainSectionCNT">
                   <Switch>
                     <Route exact path="/" component={Home}/>
+                    <Route exact path="/formularz" component={Form}/>
+                    <Route exact path="/krsInfo" component={KrsInfo}/>
+                    <Route exact path="/nipInfo" component={NipInfo}/>
+                    <Route exact path="/regonInfo" component={RegonInfo}/>
                     <Route component={NoMatch}/>
                   </Switch>
                 </div>
-              </nav>
+              </section>
+              <footer>
+                <div className="footerCNT">
+                  <NavLink to="/formularz" style={{ textDecoration: 'none'}} ><div className="allfooterBTNs footerBTN-1"><p>Idź do formularza</p></div></NavLink>
+                  <NavLink to="/krsInfo" style={{ textDecoration: 'none'}} ><div className="allfooterBTNs footerBTN-2"><p>Co to jest KRS?</p></div></NavLink>
+                  <NavLink to="/nipInfo" style={{ textDecoration: 'none'}} ><div className="allfooterBTNs footerBTN-3"><p>Co to jest NIP?</p></div></NavLink>
+                  <NavLink to="/regonInfo" style={{ textDecoration: 'none'}} ><div className="allfooterBTNs footerBTN-4"><p>Co to jest REGON?</p></div></NavLink>
+                </div>
+                <div style={{x:0, y:0,width:"50px", height:"50px", backgroundColor:"red"}}></div>
+              </footer>
           </div>
         </div>
       </Router>
--- src/components/home.js
+++ src/components/home.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 export class Home extends Component {
   render() {
     return (
-      <div className="mainInfoText">
-        <p>
-        </p>
+      <div className="logo-CNT">
+        <div className="logoWords-CNT">
+          <div className="firstLogo-word">
+          </div>
+          <div className="secondLogo-word">
+          </div>
+        </div>
+        <div className="magnifyingGlass-CNT">
+          <div className="magnifyingGlass">
+            <div className="magnifyingGlass-text">
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
