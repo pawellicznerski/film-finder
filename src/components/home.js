8846857b988f09c85e1beaa2b2e08ccaed5f51db
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-08-05 14:11:20 +0200

    Basic structure part 4

--- src/components/home.js
+++ src/components/home.js
@@ -4,15 +4,16 @@ export class Home extends Component {
   render() {
     return (
       <div className="logo-CNT">
-        <div className="logoWords-CNT">
           <div className="firstLogo-word">
           </div>
+          <div className="secondLogo-CNT">
+
           <div className="secondLogo-word">
           </div>
         </div>
         <div className="magnifyingGlass-CNT">
           <div className="magnifyingGlass">
-            <div className="magnifyingGlass-text">
+            <div className="magnifyingGlass-stick">
             </div>
           </div>
         </div>
