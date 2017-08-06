0ed7202679041aea1157b430d744e81fbeededc1
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-08-06 12:07:27 +0200

    Adding responsive home animation

--- src/components/home.js
+++ src/components/home.js
@@ -3,20 +3,19 @@ import React, { Component } from 'react';
 export class Home extends Component {
   render() {
     return (
-      <div className="logo-CNT">
+      <div>
           <div className="firstLogo-word">
           </div>
           <div className="secondLogo-CNT">
-
-          <div className="secondLogo-word">
-          </div>
-        </div>
-        <div className="magnifyingGlass-CNT">
-          <div className="magnifyingGlass">
-            <div className="magnifyingGlass-stick">
+            <div className="secondLogo-word">
             </div>
           </div>
-        </div>
+          <div className="magnifyingGlass-CNT">
+            <div className="magnifyingGlass">
+              <div className="magnifyingGlass-stick">
+              </div>
+            </div>
+          </div>
       </div>
     );
   }
