a6179e9a778e60d0dcfa692a2798b32eadd02355
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-07 11:28:02 +0200

    improving intro_logo

--- src/components/finder-app/main/main-content/intro/intro.js
+++ src/components/finder-app/main/main-content/intro/intro.js
@@ -4,18 +4,15 @@ export class Intro extends Component {
   render() {
     return (
       <div  className="intro">
-          <div className="intro__first-word">
+        <div className="intro__words">
+          <div className="intro__words_first"></div>
+          <div className="intro__words_second"></div>
+        </div>
+        <div className="intro__magnifyingGlass-container">
+          <div className="intro__magnifyingGlass">
+            <div className="intro__magnifyingGlass__stick"></div>
           </div>
-          <div className="intro__second-word-container">
-            <div className="intro__second-word">
-            </div>
-          </div>
-          <div className="intro__magnifyingGlass-container">
-            <div className="intro__magnifyingGlass">
-              <div className="intro__magnifyingGlass__stick">
-              </div>
-            </div>
-          </div>
+        </div>
       </div>
     );
   }
