90a549db16eb22bde2f9dcb9a33cc369eb3b93ba
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-15 08:44:39 +0200

    changing react router

--- src/components/finder-app/finder-app.js
+++ src/components/finder-app/finder-app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router,Route, Switch,NavLink } from 'react-router-dom';
 
 import { Intro}  from './main/main-content/intro/intro.js';
 import { Form }  from './main/main-content/form/form.js';
