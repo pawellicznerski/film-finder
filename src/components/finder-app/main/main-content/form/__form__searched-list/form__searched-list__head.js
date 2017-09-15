b06f948b21c213edb1e6c9ea211f1b191098d2b0
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-15 07:45:48 +0200

    removing some stuff

--- src/components/finder-app/main/main-content/form/__form__searched-list/form__searched-list__head.js
+++ src/components/finder-app/main/main-content/form/__form__searched-list/form__searched-list__head.js
@@ -5,11 +5,11 @@ export class SearchedListHeader extends Component {
         return (
           <thead  className="form__searched-list__head">
               <tr className="form__searched-list__head__row">
-                  <th className="form__searched-list__head__row__el form__searched-list__head__row__el_no">No.</th>
+                  <th className="form__searched-list__head__row__el">No.</th>
                   <th className="form__searched-list__head__row__el form__searched-list__head__row__el_title">Title</th>
-                  <th className="form__searched-list__head__row__el form__searched-list__head__row__el_release">Year</th>
-                  <th className="form__searched-list__head__row__el form__searched-list__head__row__el_lang">Lang</th>
-                  <th className="form__searched-list__head__row__el form__searched-list__head__row__el_vote">vote</th>
+                  <th className="form__searched-list__head__row__el">Year</th>
+                  <th className="form__searched-list__head__row__el">Lang</th>
+                  <th className="form__searched-list__head__row__el">vote</th>
                   <th className="form__searched-list__head__row__el form__searched-list__head__row__el_ori-title">original title</th>
               </tr>
           </thead>
